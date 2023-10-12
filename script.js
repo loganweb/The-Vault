d86f0ed2b500c4505dcdb0218915baef1870a1eb
loganwe <91347950+loganwe@users.noreply.github.com>
2023-10-12 12:04:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+let cpassword="vauLt"
 document.getElementById("button").onclick = function () {
   let password = document.getElementById("password").value;
-  let cpassword="vauLt"
   if (password === cpassword) {
-    document.write(
-      '<link href="style.css" rel="stylesheet" />  <a href="vault.html">open vault</a>'
-    );
+    window.location.href="vault.html";
   }
 };
+document.addEventListener("keypress",e=>{
+if(e.key="Enter"){
+  let password = document.getElementById("password").value;
+  if (password === cpassword) {
+    window.location.href="vault.html";
+  }
+}
+})
