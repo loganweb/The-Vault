15ca9ce7c6671b82ed6c7893927dafb0bbb635c3
Logan Webb <91347950+loganwe@users.noreply.github.com>
2024-03-11 21:26:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let cpassword="vauLt"
+let c=0
 document.getElementById("button").onclick = function () {
   let password = document.getElementById("password").value;
   if (password === cpassword) {
     window.location.href="vault.html";
+  }else{
+    c++
+    document.getElementById("a").value=c
   }
 };
 document.addEventListener("keypress",e=>{
